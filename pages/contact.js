da5895682ca29633a46e9d2a6f95ac704d497314
Axel <axosar@gmail.com>
2019-12-11 12:58:06 -0600

    Routes con archivos

---
+++ pages/contact.js
@@ -0,0 +1,7 @@
+const Contact = () => (
+  <div>
+    <h1>CONTACT PAGE</h1>
+  </div>
+);
+
+export default Contact;
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+const Index = () => (
+  <div>
+    <h1>Hola Amigos</h1>
+  </div>
+);
+
+export default Index;
