248c01854097242f6d7428933c3666ebece5d760
Axel <axosar@gmail.com>
2019-12-11 14:10:56 -0600

    Fetch

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
-const Index = () => (
-  <div>
-    <h1>Hola Amigos</h1>
-  </div>
-);
+import React, { Component } from 'react';
+
+class Index extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+  static async getInitialProps({ req }) {
+    const userAgent = req ? req.headers['user-agent'] : navigator.userAgent;
+    return { userAgent }
+  }
+  render(){
+    return(
+      <div>
+        <h1>Hola Amigos {this.props.userAgent}</h1>
+      </div>
+    )
+  }
+}
 
 export default Index;
