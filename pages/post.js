08c87f066c3b07ad80a2692cb1df718d8dc515d3
Axel <axosar@gmail.com>
2019-12-11 18:32:30 -0600

    Adding server side route

--- pages/post.js
+++ pages/post.js
@@ -1,9 +1,24 @@
-import { withRouter } from 'next/router';
-
-const Post = (props) => (
-  <h1>POST page # {props.router.query.id}</h1>
-);
-//Post.getInitialProps = async ({query}) => {
-//  return query;
-//};
-export default withRouter(Post);
+import axios from 'axios';
+const Post = ({id, comments}) => (
+  <div>
+    <h1>POST page # {id}</h1>
+    <h2>COMMENTS</h2>
+    <hr/>
+    <ul>
+    {comments.map(comment => (
+      <li key={comment.id}>
+        <p>Name: {comment.name}</p>
+        <p>Comment: {comment.body}</p>
+      </li>
+    ))}
+    </ul>
+  </div>
+)
+Post.getInitialProps = async ({query}) => {
+  const res = await axios.get(
+    `https://jsonplaceholder.typicode.com/comments?postId=${query.id}`
+  );
+  const {data} = res;
+  return {...query, comments: data};
+};
+export default Post;
